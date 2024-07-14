53f6a2872c151b864a9a203b76439af7e08321a7
jasone647 <120228315+jasone647@users.noreply.github.com>
2024-07-14 06:11:27 -0400

    Update p=5.py

--- p=5.py
+++ p=5.py
@@ -1,22 +1,16 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 p=5
 N=70
 R.<q> = PowerSeriesRing(QQ)
 
+#Define Eisenstein series
 E2 = 1-24*sum([sigma(n)*q^n for n in range(1,N+1)])
 E4 = 1+240*sum([sigma(n,3)*q^n for n in range(1,N+1)])
 E6 = 1-504*sum([sigma(n,5)*q^n for n in range(1,N+1)])
-
-
 E2s=E2-p^(2-1)*E2(q^p)
 E4s=E4-p^(4-1)*E4(q^p)
 E6s=E6-p^(6-1)*E6(q^p)
 
+#Define hauptmodul and modular derivatives
 def D(f,k):
     return q*f.derivative(q)-(k/12)*E2*f
 
@@ -31,12 +25,14 @@ hthree=D(htwo,4)
 hfour=D(hthree,6)
 hfive = D(hfour,8)
 
+#List combinations of generators of each weight
 W2 = [E2s]  
 W4 = [E2s^2,E4,E4s]  
 W6 = [E2s^3,E2s*E4,E2s*E4s]  
 W8 = [E2s^4,E2s^2*E4,E2s^2*E4s,E4^2,E4s^2,]  
 W10=[E2s^5,E2s*E4^2,E2s*E4s^2,E2s^3*E4,E2s^3*E4s]  
 
+#Multiply modular derivates by elements of appropriate weight to make them weight 14, put them all into list hs to use in matrix
 hs = []
 hfour12 = [hfour*i for i in W2]
 for i in hfour12:
@@ -58,10 +54,3 @@ hs.append(hfive)
 
 M=Matrix([[xx[ii] for xx in hs] for ii in range(23)])
 show(M.rref())
-
-
-# In[ ]:
-
-
-
-
